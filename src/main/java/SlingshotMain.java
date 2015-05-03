45687d49bcba89338898a66619fa70dc9704b7ff
Aaron Bell <aaron.bell@gmail.com>
2015-05-03 11:57:44 -0700

    first steps with intellij

---
+++ src/main/java/SlingshotMain.java
@@ -0,0 +1,15 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Slingshot main.
+ * @author aaron
+ */
+public class SlingshotMain {
+    private static final Logger log = LoggerFactory.getLogger(SlingshotMain.class);
+
+    public static void main(String[] args) {
+        log.info("yas!");
+    }
+
+}
